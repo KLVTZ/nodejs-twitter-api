5fa9a60dfad71d3e58dfa3b0cf0738bbb1dda599
Justin Page <xjustinpagex@gmail.com>
2015-01-06 15:56:14 -0800

    update: rely on npm package mgnmt, use twitter module, ignore ENV vars

--- twitter.js
+++ twitter.js
@@ -1,30 +1,36 @@
-var twitter = require('ntwitter');
+// bring in your env variables
+require('dot-env');
 
-// oAuth for twitter API v1.1
+var twitter = require('twitter');
+
+// oauth for twitter api
 var twit = new twitter({
-	consumer_key: '',
-	consumer_secret: '',
-	access_token_key: '',
-	access_token_secret: ''
+	consumer_key: process.env.CONSUMER_KEY, 
+	consumer_secret: process.env.CONSUMER_SECRET,
+	access_token_key: process.env.ACCESS_TOKEN,
+	access_token_secret: process.env.ACCESS_TOKEN_SECRET 
 });
 
 // database inclusion
 var mongo = require("mongodb");
 var host = "127.0.0.1";
 var port = mongo.Connection.DEFAULT_PORT;
+
 var db = new mongo.Db("nodejs-introduction", new mongo.Server(host, port, {}), {safe: true});
 var tweetCollection;
+
+// start the connection to mongodb
 db.open(function(error){
 	console.log("We are connected! " + host + ":" + port);
 
 	db.collection("tweet", function(error, collection){
+		// tweet collection reference is in global scope
 		tweetCollection = collection;
 	});
 
 });
 
 // each stream of JSON tweet is inserted into mongo DB -
-// twitter API v1.1 is JSON default -no need to parse
 twit.stream('statuses/filter', {track: 'bieber'}, function(stream) {
 	stream.on('data', function(tweet) {
 		tweetCollection.insert(tweet, function(error) {
